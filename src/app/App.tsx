7c2c88b5ef6009d633aa36d5518048f5e3350159
Saspian <nesaspian@gmail.com>
2026-02-08 02:01:01 +0100

    Umami added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -227,6 +227,7 @@ export default function App() {
                           }}
                           whileHover={{ scale: 1.08 }}
                           whileTap={{ scale: 0.95 }}
+                          data-umami-event="valentine_yes"
                         >
                           Yes! 💕
                         </motion.button>
@@ -241,6 +242,7 @@ export default function App() {
                           }}
                           whileHover={{ scale: 1.05 }}
                           whileTap={{ scale: 0.95 }}
+                          data-umami-event="valentine_no"
                         >
                           No
                         </motion.button>
